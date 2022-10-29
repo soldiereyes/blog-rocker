00456af261cd27d4db3dad43036df41f653a1436
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2022-10-29 11:14:17 -0300

    feature: remove folders || implementation background components

---
+++ src/components/background/Background.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const Background = styled.div`
+  font-family: Rubik, sans-serif;
+  box-sizing: border-box;
+  background: linear-gradient(45deg, rgba(241, 161, 10, 1) 0%, rgba(52, 35, 3, 1) 100%);
+  background-size: 500%;
+  animation: bg-animation 7s infinite alternate;
+  @keyframes bg-animation {
+    0% {
+      background-position: left;
+    }
+    100% {
+      background-position: right;
+    }
+  }
+
+`
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './components/modalContact/Modal';
+import {Background} from "./components/background/Background";
 
- ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Background>
+            <App/>
+        </Background>
+    </React.StrictMode>
 );
 
 
